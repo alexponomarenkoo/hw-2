9d2743e66e50f757165a4fea3ba7501c50a5b585
alexponomarenkoo <el4uva4ello@gmail.com>
2023-12-18 22:16:51 +0200

    home work 2

---
+++ main.py
@@ -0,0 +1,17 @@
+.
+# ###########
+# Користувач вводить з клавіатури кількість метрів. 
+# Залежно від вибору користувача програма переводить метри милі, дюйми або ярди.
+
+ ###### task2
+
+# meters = int(input("Print meters: "))
+# what = int(input("Select a unit of measurement\n Miles - 1\n Inch - 2\n Yard - 3"))
+# if what == 1:
+#    print(f"{meters} meters = {meters*0.000621} miles")
+# elif what == 2:
+#    print(f"{meters} meters = {meters*39.37} inch")
+# elif what == 3:
+#    print(f"{meters} meters = {meters*1.093613} yard")
+#######
+#
