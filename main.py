4f4c710d21088494f7be192234904ed1b052f116
alexponomarenkoo <el4uva4ello@gmail.com>
2023-12-18 22:16:51 +0200

    completed task 1

---
+++ main.py
@@ -0,0 +1,24 @@
+# .
+# # Користувач вводить три цифри з клавіатури. Залежно від вибору користувача програма
+#  # виводить на екран максимум із трьох,
+#  # мінімум із трьох або середньоарифметичне трьох чисел.
+# n1 = int(input("Enter first number: "))
+# n2 = int(input("Enter second number: "))
+# n3 = int(input("Enter third number: "))
+#
+# if n1 > n2 > n3:
+#     print(n1)
+# elif n2 > n3 > n1:
+#     print(n2)
+# elif n3 > n2 > n1:
+#     print(n3)
+#
+# if n1 < n2 < n3:
+#     print(n1)
+# elif n2 < n3 < n1:
+#     print(n2)
+# elif n3 < n2 < n1:
+#     print(n3)
+#
+# Average = (n1+n2+n3)/3
+# print(Average)
